1f8108fc0ecc25ec483a65fe77cc9db082191397
Johan Blomgren <johan.blomgren@svenskaspel.se>
2020-05-25 18:35:00 +0200

    express and modern lint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+let db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+// app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+app.get('/:id', async (req, res) => res.send(await getItems(req.params.id)));
+app.get('/', async (req, res) => res.send(await getLists()));
+
+
+// Expose Express API as a single Cloud Function:
+exports.lists = functions.https.onRequest(app);
+
+
+const getLists = async () => {
+  const col = db.collection('lists')
+  const snapshot = await col.get();
+  const docs = snapshot.docs.map(doc => {
+    return {id: doc.id, ...doc.data()}
+  })
+  return JSON.stringify(docs);
+}
+
+const getItems = async (id) => {
+  const col = db.collection('lists').doc(id).collection('items')
+  const snapshot = await col.get();
+  const docs = snapshot.docs.map(doc => {
+    return {id: doc.id, ...doc.data()}
+  })
+  return JSON.stringify(docs);
+}
